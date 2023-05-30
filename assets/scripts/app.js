5727e5bec4f75f9f754e7c9cd3ace5dd9f84173d
Arthur Di Lascio <31333091+adlascio@users.noreply.github.com>
2023-05-30 09:14:28 -0700

    step 2 done

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,3 +17,33 @@ const product = new Product(
 );
 
 console.log("prod", product);
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    console.log("prod", this.product);
+  }
+
+  render() {
+    const li = document.createElement("li");
+    li.innerHTML = `<div>
+    <img
+      src="${this.product.img}"
+      alt="${this.product.title}"
+    />
+    <div class="product-item__content">
+      <h2>${this.product.title}</h2>
+      <h3>$${this.product.price}</h3>
+      <p>${this.product.description}</p>
+      <button>Add to Cart</button>
+    </div>
+  </div>`;
+    return li;
+  }
+}
+
+const productItem = new ProductItem(product);
+console.log("li", productItem.render());
